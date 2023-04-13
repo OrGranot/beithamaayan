a1e0d3a8e87d9132d1d19ff8e931c739b10d8ec1
Or Granot <orgranot91@gmail.com>
2023-04-13 15:02:22 +0200

    navbar scale on scroll

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 import './css/reset.scss';
 import './css/style.scss';
+const nav = document.querySelector('header')
+let scrollPos = 0
+
+
+window.addEventListener('scroll', (e) => {
+  const pos = window.pageYOffset
+  if (pos - scrollPos > 0) {
+    nav.classList.add('hide')
+  }
+  else {
+    nav.classList.remove('hide')
+  }
+  scrollPos = pos
+})
