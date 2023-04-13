411083d04fc56efb4ef1b7bb1cf76eb3fda0fe71
Or Granot <orgranot91@gmail.com>
2023-04-13 14:29:36 +0200

    new design mobile ready

--- main.js
+++ main.js
@@ -1,23 +1,2 @@
-import './style.scss'
-
-// const horizontal_content = document.querySelector('.horizontal-content')
-// const horizontal_items = gsap.utils.toArray(".panel")
-// // const horizontal_content = horizontal_content.querySelector(".horizontal-content")
-
-// gsap.to(horizontal_items, {
-//   xPercent: 200,
-//   wase: "sign.out",
-//   scrollTrigger: {
-//     trigger: horizontal_content,
-//     pin: true,
-//     scrub: 1,
-//     snap: (horizontal_items.lenght),
-//     end: () => `+=${horizontal_content.offsetHeight}`
-//     // markers: true
-//   }
-// })
-
-// console.log(horizontal_items.length - 1)
-document.getElementById('form').addEventListener('submit', (e) => {
-  e.preventDefault()
-})
+import './css/reset.scss';
+import './css/style.scss';
