dbc0d4ecf59beb94e56c38686048bee3f1976f77
Or Granot <orgranot91@gmail.com>
2023-04-24 15:28:16 +0200

    full image modal

--- main.js
+++ main.js
@@ -78,3 +78,52 @@ mm.add("(min-width: 72.5rem)", () => {
   .to('.hero .inner-glow', {width: '65%', duration: 2 }, '-=2')
   .to('.hero_text', {top: 0, duration: 1, alpha: 1}, '-=0.5');
 });
+
+
+
+const modal = document.getElementById("myModal");
+const closeBtn = modal.querySelector('.close');
+const images = document.querySelectorAll('img')
+
+images.forEach(img => {
+  img.setAttribute('tabindex', 0)
+  if (img.dataset.open == "false") {
+    return
+  }
+  img.addEventListener('click', (e) => {
+  openMoadal(e)
+  })
+  img.addEventListener('keydown', (e) => {
+    if (e.key != 'Enter' && e.key) {
+      return
+    }
+    openMoadal(e)
+  })
+})
+
+function openMoadal(e) {
+  modal.querySelector('img').src = e.target.src
+  modal.style.display = "flex";
+
+  document.addEventListener('keydown', (e) => {
+    if (e.key == 'Escape') {
+      document.removeEventListener('keydown', closeModal())
+    }
+  })
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// When the user clicks on <closeBtn> (x), close the modal
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
